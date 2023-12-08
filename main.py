d16ff16967ea899de404b4210e3007d05dfb0494
nasreenot2000 <nasreen.ds22@duk.ac.in>
2023-12-08 13:35:42 +0530

    fileupdated

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import tensorflow as tf
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from tqdm import tqdm
 from keras.utils import to_categorical
+from tqdm import tqdm
 
-image_dir='cropped'
-lional_messi_images=os.listdir(image_dir+ '/lionel_messi')
-maria_sharapova_images=os.listdir(image_dir+ '/maria_sharapova')
-roger_federer_images=os.listdir(image_dir+'/roger_federer')
-serena_williams_images=os.listdir(image_dir+'/serena_williams')
-virat_kohli_images=os.listdir(image_dir+'/virat_kohli')
+image_dir = 'cropped'
+messi = os.listdir('cropped\\lionel_messi')
+maria = os.listdir('cropped\\maria_sharapova')
+roger = os.listdir('cropped\\roger_federer')
+serena = os.listdir('cropped\\serena_williams')
+virat = os.listdir('cropped\\virat_kohli')
+
 print("--------------------------------------\n")
 
-print('The length of lional_messi_images is',len(lional_messi_images))
-print('The length of maria_sharapova_images is',len(maria_sharapova_images))
-print('The length of roger_federer_images is',len(roger_federer_images))
-print('The length of serena_williams_images is',len(serena_williams_images))
-print('The length of virat_kohli_images is',len(virat_kohli_images))
-
+print('The number of images of Lionel Messi is',len(messi))
+print('The number of images of Maria Sharapova is',len(maria))
+print('The number of images of Roger Federer is',len(roger))
+print('The number of images of Serena Williams is',len(serena))
+print('The number of images of Virat Kohli is',len(virat))
 print("--------------------------------------\n")
 
 
@@ -30,7 +30,7 @@ label=[]
 img_siz=(128,128)
 
 
-for i , image_name in tqdm(enumerate(lional_messi_images),desc="Lional_messi"):
+for i , image_name in tqdm(enumerate(messi),desc="Lionel Messi"):
     if(image_name.split('.')[1]=='png'):
         image=cv2.imread(image_dir+'/lionel_messi/'+image_name)
         image=Image.fromarray(image,'RGB')
@@ -38,8 +38,7 @@ for i , image_name in tqdm(enumerate(lional_messi_images),desc="Lional_messi"):
         dataset.append(np.array(image))
         label.append(0)
         
-        
-for i ,image_name in tqdm(enumerate(maria_sharapova_images),desc="maria_sharapova"):
+for i , image_name in tqdm(enumerate(maria),desc="Maria Sharapova"):
     if(image_name.split('.')[1]=='png'):
         image=cv2.imread(image_dir+'/maria_sharapova/'+image_name)
         image=Image.fromarray(image,'RGB')
@@ -47,7 +46,7 @@ for i ,image_name in tqdm(enumerate(maria_sharapova_images),desc="maria_sharapov
         dataset.append(np.array(image))
         label.append(1)
 
-for i ,image_name in tqdm(enumerate(roger_federer_images),desc="roger_federer"):
+for i , image_name in tqdm(enumerate(roger),desc="Roger Federer"):
     if(image_name.split('.')[1]=='png'):
         image=cv2.imread(image_dir+'/roger_federer/'+image_name)
         image=Image.fromarray(image,'RGB')
@@ -55,7 +54,7 @@ for i ,image_name in tqdm(enumerate(roger_federer_images),desc="roger_federer"):
         dataset.append(np.array(image))
         label.append(2)
 
-for i ,image_name in tqdm(enumerate(serena_williams_images),desc="serena_williams"):
+for i , image_name in tqdm(enumerate(serena),desc="Serena William"):
     if(image_name.split('.')[1]=='png'):
         image=cv2.imread(image_dir+'/serena_williams/'+image_name)
         image=Image.fromarray(image,'RGB')
@@ -63,15 +62,14 @@ for i ,image_name in tqdm(enumerate(serena_williams_images),desc="serena_william
         dataset.append(np.array(image))
         label.append(3)
 
-for i ,image_name in tqdm(enumerate(virat_kohli_images),desc="virat_kohli"):
+for i , image_name in tqdm(enumerate(virat),desc="Virat Kohli"):
     if(image_name.split('.')[1]=='png'):
         image=cv2.imread(image_dir+'/virat_kohli/'+image_name)
         image=Image.fromarray(image,'RGB')
         image=image.resize(img_siz)
         dataset.append(np.array(image))
         label.append(4)
-        
-        
+
 dataset=np.array(dataset)
 label = np.array(label)
 
@@ -87,60 +85,34 @@ x_train,x_test,y_train,y_test=train_test_split(dataset,label,test_size=0.2,rando
 print("--------------------------------------\n")
 
 print("--------------------------------------\n")
-print("Normalaising the Dataset. \n")
 
-# x_train=x_train.astype('float')/255
-# x_test=x_test.astype('float')/255 
+x_train=x_train.astype('float')/255
+x_test=x_test.astype('float')/255
+print("--------------------------------------\n")
 
-# Same step above is implemented using tensorflow functions.
+#print("Normalaising the Dataset. \n")
 
-x_train=tf.keras.utils.normalize(x_train,axis=1)
-x_test=tf.keras.utils.normalize(x_test,axis=1)
+#x_train=tf.keras.utils.normalize(x_train,axis=1)
+#x_test=tf.keras.utils.normalize(x_test,axis=1)
 
 print("--------------------------------------\n")
-img_size = (128, 128, 3)  # Adjust this according to your actual image size
-num_classes = 5  # Change this to the number of classes in your problem
-
-data_augmentation = tf.keras.Sequential([
-    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=img_size),
-    tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-    tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
-])
-
-model = tf.keras.models.Sequential([
-    data_augmentation,
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(num_classes, activation='softmax')  # Use softmax for multi-class classification
-])
-
-
-
 '''data_augmentation = tf.keras.Sequential(
    [
-     tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal",input_shape=img_siz),
+     tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal",input_shape=(128,128,3)),
      tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
      tf.keras.layers.experimental.preprocessing.RandomZoom(0.2),
    ]
- )
-
+ )'''
 
 model=tf.keras.models.Sequential([
-    data_augmentation,
     tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(128,128,3)),
     tf.keras.layers.MaxPooling2D((2,2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256,activation='relu'),
     tf.keras.layers.Dropout(.5),
     tf.keras.layers.Dense(512,activation='relu'),
-    tf.keras.layers.Dense(1,activation='sigmoid')
-])'''
-
-
+    tf.keras.layers.Dense(5,activation='softmax')
+])
 print("--------------------------------------\n")
 model.summary()
 print("--------------------------------------\n")
@@ -149,17 +121,14 @@ model.compile(optimizer='adam',
             loss='CategoricalCrossentropy',
             metrics=['accuracy'])
 
+y_train_one_hot = to_categorical(y_train,num_classes=5)
+y_test_one_hot = to_categorical(y_test,num_classes=5)
 
 print("--------------------------------------\n")
 print("Training Started.\n")
-y_train_one_hot = to_categorical(y_train, num_classes=5)
-history=model.fit(x_train,y_train_one_hot,epochs=100,batch_size =128,validation_split=0.1)
+history=model.fit(x_train,y_train_one_hot,epochs=50,batch_size =128,validation_split=0.1)
 print("Training Finished.\n")
 print("--------------------------------------\n")
-
-
-
-y_test_one_hot = to_categorical(y_test, num_classes=5)
 
 print("--------------------------------------\n")
 print("Model Evalutaion Phase.\n")
@@ -175,4 +144,33 @@ print("--------------------------------------\n")
 print("--------------------------------------\n")
 print("Model Prediction.\n")
 
+def preprocess_single_image(image_path):
+    img_size = (128, 128)
+    image = cv2.imread(image_path)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize(img_size)
+    image = np.array(image)
+    image = image.astype('float32') / 255.0
+    return image
 
+# List of paths to multiple images you want to predict
+image_paths_to_predict = [
+     'cropped\\lionel_messi\\lionel_messi1.png',
+     'cropped\\maria_sharapova\\maria_sharapova1.png',
+     'cropped\\roger_federer\\roger_federer1.png',
+     'cropped\\serena_williams\\serena_williams2.png',
+     'cropped\\virat_kohli\\virat_kohli1.png'
+    # Add more image paths as needed
+]
+
+# Preprocess and predict for each image
+for image_path in image_paths_to_predict:
+    single_image = preprocess_single_image(image_path)
+    single_image = np.expand_dims(single_image, axis=0)
+    predictions = model.predict(single_image)
+    predicted_class = np.argmax(predictions)
+    
+    class_names = ['Lionel Messi', 'Maria Sharapova', 'Roger Federer', 'Serena williams', 'Virat kohli']
+    predicted_label = class_names[predicted_class]
+
+    print(f" Predicted image: {predicted_label}")
